96cc746752fafd4643b4c8e39b5d80365f7c6ac1
Thomas Roberts <139529054+TomRob62@users.noreply.github.com>
2024-02-24 18:13:02 -0500

    Docs sobel filter
    updating description to include why the coefficient variable is needed.

--- filters.py
+++ filters.py
@@ -72,6 +72,9 @@ class Morphological_Spatial_Filters:
 
                 step 2: subtract sobel picture from original image
 
+            Note: I found that the original sobel filter alone was a very sharp, so I added the
+            coefficient variable so user's can control the degree of sharpness.
+
             Paramaters
             ----------
             orig_image: NDarray
@@ -101,7 +104,7 @@ class Morphological_Spatial_Filters:
                 sobel_vertical_left = orig_image[row-1][col-1] + 2*orig_image[row][col-1] + orig_image[row+1][col-1] - \
                     (orig_image[row-1][col+1] + 2*orig_image[row][col+1] + orig_image[row+1][col+1])
                 
-                # step 2
+                # step 2. 
                 pixel_value = orig_image[row][col] - coefficient*(sobel_vertical_left + sobel_horizontal_bottom)
 
                 # restricting maximum pixel value to prevent overflow when assigning value
